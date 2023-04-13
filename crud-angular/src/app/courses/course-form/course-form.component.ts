726641a813f3978d2f5b7b63c27efddac1e956e1
Mauriciojdk <mauriciojdk@gmail.com>
2023-04-13 07:38:21 -0300

    Formulario de criação de curso e tratamento de erro

--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,29 +1,45 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup} from "@angular/forms";
+import {CoursesService} from "../services/courses.service";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-course-form',
   templateUrl: './course-form.component.html',
   styleUrls: ['./course-form.component.scss']
 })
-export class CourseFormComponent implements OnInit{
+export class CourseFormComponent implements OnInit {
 
   form: FormGroup;
-  constructor(private formBuilder: FormBuilder) {
-    this.form = this.formBuilder.group({
-      name: [null],
-        category: [null]
-    });
+
+  constructor(private formBuilder: FormBuilder,
+              private service: CoursesService,
+              private snackBar: MatSnackBar
+) {
+  this
+.
+  form = this.formBuilder.group({
+    name: [null],
+    category: [null]
+  });
+}
+
+  ngOnInit()
+  {
   }
 
-  ngOnInit() {
+  onSubmit()
+  {
+    this.service.save(this.form.value).subscribe(result => console.log(result),
+      error => this.onError());
   }
 
-  onSubmit() {
+  onCancel(){
 
   }
 
-  onCancel() {
-
+  private onError(){
+  this.snackBar.open("Erro ao salvar curso", "", {duration: 4000});
   }
 }
+
--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -19,4 +19,8 @@ export class CoursesService {
         tap(courses => console.log(courses))
       );
   }
+
+  save(record: Course){
+  return this.httpCliente.post<Course>(this.API, record);
+  }
 }
